c9eb8b08c7d1ac41415704aa392099fb094aa845
alysondev <devalyson@gmail.com>
2022-03-10 20:54:37 -0300

    Erro no svg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from './components/NavBar';
 import './index.css';
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <h1>Pagina inicial</h1>
     </>
   )
