cabd1b0fe9d61bff12122315af59bea66dfb2f77
alysondev <devalyson@gmail.com>
2022-03-12 02:35:56 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
-<<<<<<< HEAD
-=======
-import 'bootstrap/dist/css/bootstrap.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 import { Navbar } from './components/NavBar';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
->>>>>>> fronttest
+
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <h1>Pagina inicial</h1>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Listing />} />
+          <Route path="/form" element={<Listing />} />
+            <Route path=":movieId" element={<Form />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
