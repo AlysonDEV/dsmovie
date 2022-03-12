bd0d1476c3c0d268158feff8c4762c5ce510f9af
alysondev <devalyson@gmail.com>
2022-03-12 02:23:08 -0300

    Remove vite-ssr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
-// import ssr from 'vite-plugin-ssr/plugin'
 
 
 // https://vitejs.dev/config/
-// const base = process.env.BASE_URL
 export default defineConfig({
   plugins: [
     react(),
